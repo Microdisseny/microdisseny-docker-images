6573e1c3f7a8c267f89ece4bfa75eff6409c1cc3
abusquets <abusquets@gmail.com>
2018-10-27 09:49:08 +0200

    Add py3-development images

---
+++ build.sh
@@ -0,0 +1,3 @@
+docker build -t microdisseny/py-development py-development
+docker build -t microdisseny/py3-development py3-development
+docker build -t microdisseny/py3-development-selenium py3-development-selenium
