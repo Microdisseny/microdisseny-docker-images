b1ec770642381d6080ca8d7078b72576af178eb4
Manel Clos <manelclos@gmail.com>
2019-02-15 04:17:17 +0100

    add py3-development-ubuntu-16.04 variant

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 docker build -t microdisseny/py-development py-development
 docker build -t microdisseny/py3-development py3-development
 docker build -t microdisseny/py3-development-selenium py3-development-selenium
+docker build -t microdisseny/py3-development:ubuntu-16.04 py3-development-ubuntu-16.04
