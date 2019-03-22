257f4ab7456b325314c469d48bd6a557b5da9847
Manel Clos <manelclos@gmail.com>
2019-03-22 17:02:23 +0100

    new tag for ubuntu-18.04

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 docker build -t microdisseny/py-development py-development
-docker build -t microdisseny/py3-development py3-development
+docker build -t microdisseny/py3-development -t microdisseny/py3-development:ubuntu-18.04 py3-development
 docker build -t microdisseny/py3-development-selenium py3-development-selenium
 docker build -t microdisseny/py3-development:ubuntu-16.04 py3-development-ubuntu-16.04
