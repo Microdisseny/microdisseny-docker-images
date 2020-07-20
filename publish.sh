e275c98b6fde95af00bafa182ee209d379af69b1
Manel Clos <manelclos@gmail.com>
2020-07-20 14:54:34 +0200

    use python3.8 image

---
+++ publish.sh
@@ -0,0 +1,23 @@
+cd py-development
+docker push microdisseny/py-development
+cd ..
+
+cd py3-development
+docker push microdisseny/py3-development
+cd ..
+
+cd py3-development-selenium
+docker push microdisseny/py3-development-selenium
+cd ..
+
+cd py3-development-ubuntu-16.04
+docker push microdisseny/py3-development:ubuntu-16.04
+cd ..
+
+cd py3-development-ubuntu-18.04
+docker push microdisseny/py3-development:ubuntu-18.04
+cd ..
+
+cd py3-development-ubuntu-18.04-selenium
+docker push microdisseny/py3-development:ubuntu-18.04-selenium
+cd ..
