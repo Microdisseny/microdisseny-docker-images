9b63c59405faa147cb65a6cfce7e2835076372ef
Manel Clos <manelclos@gmail.com>
2021-08-19 21:22:25 +0200

    improve scripts

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
+docker pull ubuntu:20.04
 docker build --progress=plain -t microdisseny/py3-development:ubuntu-20.04 -t microdisseny/py3-development py3-development-ubuntu-20.04
 docker build --progress=plain -t microdisseny/py3-development:ubuntu-20.04-selenium -t microdisseny/py3-development-selenium py3-development-ubuntu-20.04-selenium
+
+docker pull ubuntu:18.04
 docker build --progress=plain -t microdisseny/py3-development:ubuntu-18.04 py3-development-ubuntu-18.04
 docker build --progress=plain -t microdisseny/py3-development:ubuntu-18.04-selenium py3-development-ubuntu-18.04-selenium
+
+docker pull ubuntu:16.04
 docker build --progress=plain -t microdisseny/py3-development:ubuntu-16.04 py3-development-ubuntu-16.04
+
 docker build --progress=plain -t microdisseny/py-development py-development
