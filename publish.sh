7741e1e22964e3f9d82d12754d5947afb96a7234
Manel Clos <manelclos@gmail.com>
2021-08-19 21:22:25 +0200

    improve scripts

--- publish.sh
+++ publish.sh
@@ -1,23 +1,10 @@
-cd py-development
-docker push microdisseny/py-development
-cd ..
-
-cd py3-development-ubuntu-16.04
-docker push microdisseny/py3-development:ubuntu-16.04
-cd ..
-
-cd py3-development-ubuntu-18.04
-docker push microdisseny/py3-development:ubuntu-18.04
-cd ..
-
-cd py3-development-ubuntu-18.04-selenium
-docker push microdisseny/py3-development:ubuntu-18.04-selenium
-cd ..
-
-cd py3-development-ubuntu-20.04
-docker push microdisseny/py3-development:ubuntu-20.04
-cd ..
-
-cd py3-development-ubuntu-20.04-selenium
-docker push microdisseny/py3-development:ubuntu-20.04-selenium
-cd ..
+for IMAGE in \
+  microdisseny/py3-development:ubuntu-20.04 \
+  microdisseny/py3-development:ubuntu-20.04-selenium \
+  microdisseny/py3-development:ubuntu-18.04 \
+  microdisseny/py3-development:ubuntu-18.04-selenium \
+  microdisseny/py3-development:ubuntu-16.04
+do
+  echo "\n* $IMAGE"
+  docker push $IMAGE
+done
