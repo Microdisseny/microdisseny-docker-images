3321d7ced14043616ade95bcbf8ba6941f2bfce0
Ainhoa Vázquez Zarza <av@microdisseny.com>
2021-11-29 12:12:18 +0100

    Crear imatge node

--- publish.sh
+++ publish.sh
@@ -1,6 +1,7 @@
 for IMAGE in \
   microdisseny/py3-development:ubuntu-20.04 \
   microdisseny/py3-development:ubuntu-20.04-selenium \
+  microdisseny/py3-development:ubuntu-20.04-node \
   microdisseny/py3-development:ubuntu-18.04 \
   microdisseny/py3-development:ubuntu-18.04-selenium \
   microdisseny/py3-development:ubuntu-16.04
