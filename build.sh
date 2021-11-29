187d5ab4956441a4a0676616dbbc73f04d8cb2a9
Ainhoa Vázquez Zarza <av@microdisseny.com>
2021-11-29 12:12:18 +0100

    Crear imatge node

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 docker pull ubuntu:20.04
 docker build --progress=plain -t microdisseny/py3-development:ubuntu-20.04 -t microdisseny/py3-development py3-development-ubuntu-20.04
 docker build --progress=plain -t microdisseny/py3-development:ubuntu-20.04-selenium -t microdisseny/py3-development-selenium py3-development-ubuntu-20.04-selenium
+docker build --progress=plain -t microdisseny/py3-development:ubuntu-20.04-node -t microdisseny/py3-development-node py3-development-ubuntu-20.04-node
 
 docker pull ubuntu:18.04
 docker build --progress=plain -t microdisseny/py3-development:ubuntu-18.04 py3-development-ubuntu-18.04
