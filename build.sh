9138cd9d2199a3b705840f30d1406ceb78578f58
Manel Clos <manelclos@gmail.com>
2022-04-07 15:09:23 +0200

    add ubuntu:22.04 builds

--- build.sh
+++ build.sh
@@ -1,7 +1,15 @@
+#! /bin/bash
+set -e
+
+docker pull ubuntu:22.04
+docker build --progress=plain -t microdisseny/py3-development:ubuntu-22.04 py3-development-ubuntu-22.04
+docker build --progress=plain -t microdisseny/py3-development:ubuntu-22.04-selenium py3-development-ubuntu-22.04-selenium
+docker build --progress=plain -t microdisseny/py3-development:ubuntu-22.04-node py3-development-ubuntu-22.04-node
+
 docker pull ubuntu:20.04
-docker build --progress=plain -t microdisseny/py3-development:ubuntu-20.04 -t microdisseny/py3-development py3-development-ubuntu-20.04
-docker build --progress=plain -t microdisseny/py3-development:ubuntu-20.04-selenium -t microdisseny/py3-development-selenium py3-development-ubuntu-20.04-selenium
-docker build --progress=plain -t microdisseny/py3-development:ubuntu-20.04-node -t microdisseny/py3-development-node py3-development-ubuntu-20.04-node
+docker build --progress=plain -t microdisseny/py3-development:ubuntu-20.04 py3-development-ubuntu-20.04
+docker build --progress=plain -t microdisseny/py3-development:ubuntu-20.04-selenium py3-development-ubuntu-20.04-selenium
+docker build --progress=plain -t microdisseny/py3-development:ubuntu-20.04-node py3-development-ubuntu-20.04-node
 
 docker pull ubuntu:18.04
 docker build --progress=plain -t microdisseny/py3-development:ubuntu-18.04 py3-development-ubuntu-18.04
--- publish.sh
+++ publish.sh
@@ -1,4 +1,7 @@
 for IMAGE in \
+  microdisseny/py3-development:ubuntu-22.04 \
+  microdisseny/py3-development:ubuntu-22.04-selenium \
+  microdisseny/py3-development:ubuntu-22.04-node \
   microdisseny/py3-development:ubuntu-20.04 \
   microdisseny/py3-development:ubuntu-20.04-selenium \
   microdisseny/py3-development:ubuntu-20.04-node \
