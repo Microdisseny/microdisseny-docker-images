586dda47dbf111e0295774918d27a9ad973bd8e5
Manel Clos <manelclos@gmail.com>
2023-07-25 16:00:47 +0200

    add darker and update versions

--- build.sh
+++ build.sh
@@ -14,5 +14,3 @@ docker build --progress=plain -t microdisseny/py3-development:ubuntu-20.04-node 
 docker pull ubuntu:18.04
 docker build --progress=plain -t microdisseny/py3-development:ubuntu-18.04 py3-development-ubuntu-18.04
 docker build --progress=plain -t microdisseny/py3-development:ubuntu-18.04-selenium py3-development-ubuntu-18.04-selenium
-
-docker build --progress=plain -t microdisseny/py-development py-development
