57690ee7827034aa9738a9a7ee74317c2bb214b2
Jordi Gomara Cruz <jg@microdisseny.com>
2025-06-10 14:42:53 +0200

    fix ubuntu images

--- build.sh
+++ build.sh
@@ -2,18 +2,18 @@
 set -e
 
 docker pull ubuntu:24.04
-docker build --progress=plain -t microdisseny/py3-development:ubuntu-24.04 py3-development-ubuntu-24.04
+docker build --progress=plain -t jgcz/microdisseny-py3-development:ubuntu-24.04 py3-development-ubuntu-24.04
 
 docker pull ubuntu:22.04
-docker build --progress=plain -t microdisseny/py3-development:ubuntu-22.04 py3-development-ubuntu-22.04
-docker build --progress=plain -t microdisseny/py3-development:ubuntu-22.04-selenium py3-development-ubuntu-22.04-selenium
-docker build --progress=plain -t microdisseny/py3-development:ubuntu-22.04-node py3-development-ubuntu-22.04-node
+docker build --progress=plain -t jgcz/microdisseny-py3-development:ubuntu-22.04 py3-development-ubuntu-22.04
+docker build --progress=plain -t jgcz/microdisseny-py3-development:ubuntu-22.04-selenium py3-development-ubuntu-22.04-selenium
+docker build --progress=plain -t jgcz/microdisseny-py3-development:ubuntu-22.04-node py3-development-ubuntu-22.04-node
 
 docker pull ubuntu:20.04
-docker build --progress=plain -t microdisseny/py3-development:ubuntu-20.04 py3-development-ubuntu-20.04
-docker build --progress=plain -t microdisseny/py3-development:ubuntu-20.04-selenium py3-development-ubuntu-20.04-selenium
-docker build --progress=plain -t microdisseny/py3-development:ubuntu-20.04-node py3-development-ubuntu-20.04-node
+docker build --progress=plain -t jgcz/microdisseny-py3-development:ubuntu-20.04 py3-development-ubuntu-20.04
+docker build --progress=plain -t jgcz/microdisseny-py3-development:ubuntu-20.04-selenium py3-development-ubuntu-20.04-selenium
+docker build --progress=plain -t jgcz/microdisseny-py3-development:ubuntu-20.04-node py3-development-ubuntu-20.04-node
 
 docker pull ubuntu:18.04
-docker build --progress=plain -t microdisseny/py3-development:ubuntu-18.04 py3-development-ubuntu-18.04
-docker build --progress=plain -t microdisseny/py3-development:ubuntu-18.04-selenium py3-development-ubuntu-18.04-selenium
+docker build --progress=plain -t jgcz/microdisseny-py3-development:ubuntu-18.04 py3-development-ubuntu-18.04
+docker build --progress=plain -t jgcz/microdisseny-py3-development:ubuntu-18.04-selenium py3-development-ubuntu-18.04-selenium
--- publish.sh
+++ publish.sh
@@ -1,13 +1,13 @@
 for IMAGE in \
-  microdisseny/py3-development:ubuntu-24.04 \
-  microdisseny/py3-development:ubuntu-22.04 \
-  microdisseny/py3-development:ubuntu-22.04-selenium \
-  microdisseny/py3-development:ubuntu-22.04-node \
-  microdisseny/py3-development:ubuntu-20.04 \
-  microdisseny/py3-development:ubuntu-20.04-selenium \
-  microdisseny/py3-development:ubuntu-20.04-node \
-  microdisseny/py3-development:ubuntu-18.04 \
-  microdisseny/py3-development:ubuntu-18.04-selenium
+  jgcz/microdisseny-py3-development:ubuntu-24.04 \
+  jgcz/microdisseny-py3-development:ubuntu-22.04 \
+  jgcz/microdisseny-py3-development:ubuntu-22.04-selenium \
+  jgcz/microdisseny-py3-development:ubuntu-22.04-node \
+  jgcz/microdisseny-py3-development:ubuntu-20.04 \
+  jgcz/microdisseny-py3-development:ubuntu-20.04-selenium \
+  jgcz/microdisseny-py3-development:ubuntu-20.04-node \
+  jgcz/microdisseny-py3-development:ubuntu-18.04 \
+  jgcz/microdisseny-py3-development:ubuntu-18.04-selenium
 do
   echo "\n* $IMAGE"
   docker push $IMAGE
